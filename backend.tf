c4b594c744006f366ab558d4d4f003a42886a10b
Akinyem <99253992+Akinyem@users.noreply.github.com>
2022-08-04 20:09:27 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
+    bucket = "yemi-sharing-bucket"
     key    = "avinash/s3file-3.tfstate"
     region = "us-west-2"
     profile = "jespo"
